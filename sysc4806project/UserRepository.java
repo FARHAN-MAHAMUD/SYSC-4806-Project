57e9c7359b5e6090addd31f44d22ceeb3b93b2ac
ihasnaou <91300614+ihasnaou@users.noreply.github.com>
2023-11-12 17:10:13 -0500

    Delete sysc4806project directory

--- sysc4806project/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sysc4806project;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Book, Long> {
-
-    User findById(long id);
-}
