329ee85eefbb00f599ba333b0674298a4329ca38
patrick <patrickvafaie@cmail.carleton.ca>
2023-11-19 16:56:24 -0500

    Created test security config, no longer need to login if imported in tests

--- src/test/java/com/example/sysc4806project/RecommendationControllerTest.java
+++ src/test/java/com/example/sysc4806project/RecommendationControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestSecurityConfig.class)
 class RecommendationControllerTest {
 
     @Autowired
